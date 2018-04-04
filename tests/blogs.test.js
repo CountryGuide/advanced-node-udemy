7ba2edc9b10db9a62131a6941e4323fe50b00349
yakovlev <ivanvyakovlev@gmail.com>
2018-04-04 16:22:09 +0300

    WIP: blogs testing

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,1 +1,40 @@
+const Page = require('./helpers/page');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('User');
+
+
+let page;
+
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
+afterAll(async () => {
+    await User.remove(
+        {
+            displayName: 'Test'
+        },
+        err => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Removed all test users from db');
+            }
+        });
+
+});
+
+test('When logged in, can see blog create form', async () => {
+    await page.login();
+    await page.click('a[href="/blogs/new"]');
+
+    const title = await page.getContent('form label');
+
+    expect(title).toEqual('Blog Title');
+});
