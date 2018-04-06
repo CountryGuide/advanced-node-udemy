34f7e8e23992aaafd5e7c94330b0d64f1af04fd0
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2018-04-06 15:56:42 +0300

    FEAT: CI

---
+++ config/ci.js
@@ -0,0 +1,7 @@
+module.exports = {
+        googleClientID:     '832023074877-0t0itlqfaq9knoa6crgqp1e46fnh59nn.apps.googleusercontent.com',
+        googleClientSecret: 'NBUgyzIKOVuQXa2v_qmEVh2W',
+        mongoURI:           'mongodb://127.0.0.1:27017/blog_ci',
+        cookieKey:          'PS43LFlfdd5Bf45f7',
+        redisUrl:           'redis://127.0.0.1:6379'
+};
